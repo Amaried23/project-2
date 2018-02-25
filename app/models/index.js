a7cd11e56ea6bf5fc8777018f2047607c8d44bf3
Sean Kelly <spkellydev@gmail.com>
2018-02-25 16:22:04 -0500

    platform agnostic model config

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,14 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\\config\\config.json')[env];
+var config;
+
+if (process.platform !== "win32") {
+  config = require(__dirname + '/../config/config.json')[env];
+} else {
+  config = require(__dirname + '/..\\config\\config.json')[env];
+}
+
 var db        = {};
 
 if (config.use_env_variable) {
