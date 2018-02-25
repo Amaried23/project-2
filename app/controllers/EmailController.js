348357bfaa0b2b62f3b214b8dcb20c42f6953073
Sean Kelly <spkellydev@gmail.com>
2018-02-24 21:01:00 -0500

    handlebars support added for email notification

--- app/controllers/EmailController.js
+++ app/controllers/EmailController.js
@@ -1,22 +1,44 @@
-const express = require('express')
-const router = express.Router()
-let keys = require('../config/keys')
+const express = require('express');
+const router = express.Router();
+let keys = require('../config/keys');
 const Mailgun = require('mailgun').Mailgun;
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+
+const auth = {
+    auth: {
+      api_key: keys.mailgun,
+      domain: 'sandbox643fe2531cc54b16b93ee92e1a51ea94.mailgun.org'
+    },
+    proxy: 'http://user:pass@localhost:8080' // optional proxy, default is false
+}
+
 
 router.post('/email', (req, res) => {
+    let nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
-    console.log(req.body.text)
+    let text = {
+        text: req.body.text
+    }
 
+    nodemailerMailgun.sendMail({
+        from: 'myemail@example.com',
+        to: 'spkellydev@gmail.com', // An array if you have multiple recipients.
+        subject: 'Hey you, awesome!',
+        template: {
+          name: 'app/views/email.hbs',
+          engine: 'handlebars',
+          context: text
+        }
+      }, function (err, info) {
+        if (err) {
+          console.log('Error: ' + err);
+        }
+        else {
+            console.log('Response: ' + JSON.stringify(info));
+        }
+      });
 
-    var mg = new Mailgun(keys.mailgun);
-    mg.sendText('example@example.com', ['Recipient 1 <spkellydev@gmail.com>'],
-      'Email from Helping Hand',
-      req.body.text,
-      'noreply@example.com', {},
-      function(err) {
-        if (err) console.log('Oh noes: ' + err);
-        else     console.log('Email Sent');
-    });
     res.redirect('/login')
 })
 
