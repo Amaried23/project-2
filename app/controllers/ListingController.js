5bb8f870ce4a99198925aead18cfb31479dc465f
Sean Kelly <spkellydev@gmail.com>
2018-02-26 19:27:26 -0500

    adding support for a zip code query

--- app/controllers/ListingController.js
+++ app/controllers/ListingController.js
@@ -20,28 +20,35 @@ class ListingController {
         return [hosts, pages]
     }
 
-    async locate(lat, lng, limit) {
-        let located = await db.sequelize.query('SELECT id, first_name, X(location) AS \'latitude\', Y(location) AS \'longitude\',(GLength(LineStringFromWKB(LineString(location,GeomFromText(\'POINT('+lat +' ' +lat+')\'))))) AS distance FROM hosts ORDER BY distance ASC LIMIT ' + limit + ';').then(data => {
-            console.log(data)
+    async byLocation(lat, lng, limit) {
+        return await db.sequelize.query('SELECT id, first_name, X(location) AS \'latitude\', Y(location) AS \'longitude\',(GLength(LineStringFromWKB(LineString(location,GeomFromText(\'POINT('+lat +' ' +lat+')\'))))) AS distance FROM hosts ORDER BY distance ASC LIMIT ' + limit + ';').then((data) => {
+            return data
+        }).catch(err => {
+            if (err) console.log(err)
         })
-
-        return located
 
         // var lat = parseFloat("-37.1196");
         // var lng = parseFloat("42.6733");
         // var attributes = Object.keys(Hosts.attributes);
-        // var location = sequelize.literal(`GeomFromText('POINT(${lng} ${lat})')`);
-        // var distance = sequelize.fn('ST_Distance_Sphere', sequelize.col('location'), location);
+        // var location = db.sequelize.literal(`GeomFromText('POINT(${lng} ${lat})')`);
+        // var distance = db.sequelize.fn('ST_Distance_Sphere', db.sequelize.literal('location'), location);
         // attributes.push([distance,'distance']);
 
-        // let located = await Hosts.findAll({
-        // attributes: attributes,
-        // order: distance,
-        // where: sequelize.where(distance, {$lte: 5000}),
-        // logging: console.log
-        // }).catch((err) => {
-        //     console.log(err)
-        // })
+        // try {
+        //     return await Hosts.findAll({
+        //     attributes: attributes,
+        //     order: distance,
+        //     where: db.sequelize.where(distance, {$lte: 10000}),
+        //     logging: console.log
+        //     }).then(data => {
+        //         return data
+        //     }).catch(err => {
+        //         console.log(err)
+        //     })
+        // } catch(err) {
+        //     return err
+        // }
+
 
         // return located
     }
--- app/routes/listingRoutes.js
+++ app/routes/listingRoutes.js
@@ -14,12 +14,18 @@ router.get('/listings/:page', async (req, res) => {
     let page = req.params.page
     let limit = 5;
 
-    let located = await ListingController.locate(51.5177, -0.0968, 4)
-
+    
     
     if (req.query) {
         if (req.query.limit) {
             limit = JSON.parse(req.query.limit)
+        }
+        
+        if (req.query.zip) {
+            let zip = JSON.parse(req.query.zip)
+            //get lat/lng from zip code
+            let located = await ListingController.byLocation(51.5177, -0.0968, limit)
+            console.log(located)
         }
     }
 
@@ -39,6 +45,7 @@ router.get('/listings/:page', async (req, res) => {
         hosts: paginatedHosts,
         pages
     })
+
 })
 
 module.exports = router
